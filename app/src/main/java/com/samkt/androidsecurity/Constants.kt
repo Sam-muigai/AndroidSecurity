f7e2445d3621cb70c5407ad33bca6f68589e9770
Sam <sammuigai880@gmail.com>
2024-07-14 20:30:29 +0300

    Initial commit

---
+++ app/src/main/java/com/samkt/androidsecurity/Constants.kt
@@ -0,0 +1,5 @@
+package com.samkt.androidsecurity
+
+object Constants {
+    const val API_KEY = "fbe720bb652d4a5e93ce64fff1dfe814"
+}
---
+++ app/src/main/java/com/samkt/androidsecurity/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.samkt.androidsecurity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.samkt.androidsecurity.ui.theme.AndroidSecurityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidSecurityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        Text(text = Constants.API_KEY)
+                    }
+                }
+            }
+        }
+    }
+}
+
